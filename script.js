0dcb43ef5e396eb51179e135b7935c3fc296b3ca
TUSHAR P BISANE <tpbisane7@gmail.com>
2025-06-14 10:34:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.getElementById("alertButton");
+
+  button.addEventListener("click", function () {
+    alert("🚀 Great job! You just triggered a JavaScript event.");
+  });
+});
